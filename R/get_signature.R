31d0f0877b73ee34efa3a736232c4cbf166953df
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2019-12-11 17:00:41 -0500

    Initial commit

---
+++ R/get_signature.R
@@ -0,0 +1,57 @@
+# get_signature.R
+# -----------------------------------------------------------------------------
+# Author:             Bahman Afsari, Albert Kuo
+# Date last modified: Dec 10, 2019
+#
+# (Export) Function to calculate signature
+
+# library(dplyr)
+
+#' Function to obtain a SuperSig
+#' 
+#' Generate a tissue-specific SuperSig for a given dataset of mutations and
+#' exposure factor. Returns the SuperSig and a classification model trained with
+#' the SuperSig.
+#' 
+#' @param dt a data frame of mutations
+#' @param factor the factor/exposure (e.g. "age", "smoking")
+#' 
+#' @import dplyr
+#' 
+#' @export
+#' 
+#' @return \code{get_signature} returns a list of several elements:
+#' \itemize{
+#' \item \code{Signature} is a vector of the signature, i.e. the features that
+#' comprise the signature and their difference in mean rates (or counts if the factor
+#' is "age")
+#' \item \code{Features} is a list of features that comprise the signature and 
+#' their representation in terms of the fundamental (trinucleotide) mutations
+#' \item \code{Apparent AUC} is the apparent AUC obtained within the provided data
+#' using the generated SuperSig
+#' \item \code{Model} is the logistic regression model trained on the data and
+#' the generated SuperSig
+#' }
+#' 
+
+get_signature <- function(dt, factor){
+  # Get features
+  features_out = FeatureSelection(dt, factor)
+  
+  # Get apparent AUC and model
+  classification_out = MyLDAEnvClassifier(dt = features_out$dt_new,
+                                          factor = factor,
+                                          classifier = c("Logit"),
+                                          keep_classifier = T,
+                                          features_selected = features_out$features_selected,
+                                          select_n = features_out$select_n)
+  
+  out = list()
+  
+  out["Signature"] = classification_out$signature$mean_diffs
+  out["Features"] = features_out$new_partition[names(out["Signature"])]
+  out["Apparent AUC"] = classification_out$auc
+  out["Model"] = classification_out$classifier
+  
+  return(out)
+}
