316abc4c6a04189a2d10fcc66cba4ab929e260f5
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-03-12 15:02:42 -0400

    Add more samples to example_dt

--- data-raw/SaveExternalData.R
+++ data-raw/SaveExternalData.R
@@ -5,16 +5,28 @@
 # Save rds files to external data for R package
 
 # Generate example VCF data for vignette
-example_dt = data.frame(sample_id = rep(1:2, each = 5),
-                        age = rep(c(50, 55), each = 5),
+example_dt = data.frame(sample_id = rep(1:5, each = 5),
+                        age = rep(c(50, 55, 72, 53, 48), each = 5),
                         chromosome = c("chr1", "chr2", "chr7", "chr7", "chr19",
-                                       "chr1", "chr3", "chr9", "chr16", "chr22"),
+                                       "chr1", "chr3", "chr9", "chr16", "chr22",
+                                       "chr1", "chr4", "chr2", "chr11", "chr2",
+                                       "chr1", "chr18", "chr6", "chr6", "chr1",
+                                       "chr1", "chr4", "chr19", "chr6", "chr10"),
                         position = c(94447621, 202005395, 20784978, 87179255, 1059712,
-                                     76226977, 38180872, 139905080, 1562631, 42189307),
+                                     76226977, 38180872, 139905080, 1562631, 42189307,
+                                     94447621, 202005395, 20784978, 87179255, 1059712,
+                                     76226977, 38180872, 139905080, 1562631, 42189307,
+                                     94447621, 202005395, 20784978, 87179255, 1059712,),
                         from = c("G", "A", "T", "C", "G",
-                                 "T", "C", "G", "G", "C"),
+                                 "T", "C", "G", "G", "C",
+                                 "T", "A", "T", "A", "T",
+                                 "G", "C", "G", "G", "G",
+                                 "A", "C", "T", "G", "G"),
                         to = c("C", "C", "A", "G", "T",
-                               "C", "G", "T", "T", "A"))
+                               "C", "G", "T", "T", "A",
+                               "C", "C", "A", "G", "T",
+                               "C", "G", "T", "T", "A",
+                               "C", "C", "A", "G", "T"))
 
 # Create transform_muts_vec, a named vector that converts mutations to a standard format,
 # i.e. its fundamental mutation must begin with "C" or T"
