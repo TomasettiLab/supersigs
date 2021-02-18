ef31b316569b384e11821998093e2838716b96ee
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2021-02-18 17:15:11 -0500

    Fix error message

--- tests/testthat/test_context_matters.R
+++ tests/testthat/test_context_matters.R
@@ -2,6 +2,5 @@ x1 <- tibble(x = 1)
 
 test_that("context_matters valid input", {
   expect_error(context_matters(muts_df = x1),
-               "Column names of input data are not correct 
-              in context_matters")
+               "Column names.*")
 })
