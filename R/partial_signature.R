91c999545066446d0df796e901b54f79a073c742
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2021-02-25 16:46:38 -0500

    Initial commit

---
+++ R/partial_signature.R
@@ -0,0 +1,63 @@
+# partial_signature.R
+# -----------------------------------------------------------------------------
+# Author: Albert Kuo
+# Date last modified: Feb 25, 2021
+#
+# (Export) Function to remove signature contribution from data
+
+# library(dplyr)
+
+#' Function to remove the contribution of a SuperSig
+#' 
+#' Remove the contribution of a SuperSig from the data and return the data.
+#' 
+#' @param dt a data frame of mutations (see vignette for details)
+#' @param object an object of class "SuperSig"
+#' 
+#' @import dplyr
+#' 
+#' @export
+#' 
+#' @return \code{predict_signature} returns the original data frame with 
+#' the contribution of a supervised signature removed
+#' 
+#' @examples
+#' 
+#' head(example_dt) # use example data from package
+#' input_dt <- make_matrix(example_dt) # convert to correct format
+#' input_dt$IndVar <- c(1, 1, 1, 0, 0) # add IndVar column
+#' supersig <- get_signature(dt = input_dt, factor = "age") # get SuperSig
+#' partial_signature(dt = input_dt, object = supersig)
+#' 
+partial_signature <- function(dt, object){
+  # Check column names of dt
+  if(is.na(match("SAMPLE_ID",toupper(colnames(dt))))){
+    stop('Input data frame missing sample_id column.')
+  } else {
+    colnames(dt)[match("SAMPLE_ID",toupper(colnames(dt)))] = "sample_id"
+  }
+  if(is.na(match("AGE",toupper(colnames(dt))))){
+    stop('Input data frame missing AGE column.')
+  } else {
+    colnames(dt)[match("AGE",toupper(colnames(dt)))] = "AGE"
+  }
+  if(is.na(match("INDVAR",toupper(colnames(dt))))){
+    stop('Input data frame missing IndVar column.')
+  } else {
+    colnames(dt)[match("INDVAR",toupper(colnames(dt)))] = "IndVar"
+  }
+  
+  # Extract slots from object
+  features <- Features(object)
+  
+  # Subtract contributions (rates*age)
+  for(i in seq_along(features)){
+    trinucleotide_features <- unname(features[[i]])
+    diff <- Signature(object)[[i]]
+    dt <- dt %>%
+      mutate_at(vars(all_of(trinucleotide_features)), ~.- diff*AGE) %>%
+      mutate_at(vars(all_of(trinucleotide_features)), ~ifelse(. < 0, 0, .))
+  }
+  
+  return(dt)
+}
